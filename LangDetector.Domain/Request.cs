c33e042247a98eaff4a9a2ac18f6bd66fedfb74c
IlyaTsiamchyk <ilia.co@mail.ru>
2016-02-11 12:52:35 +0300

    Beta

---
+++ LangDetector.Domain/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangDetector.Domain
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string QueryResult { get; set; }
+        public string QueryString { get; set; }
+    }
+}
---
+++ LangDetector.Domain/RequestInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LangDetector.Domain
+{
+    [Table("RequestsInfo")]
+    public class RequestInfo
+    {
+        [Key]
+        public string UserId { get; set; }
+        public int AmountOfQueries { get; set; }
+        public DateTime RegisterDateTime { get; set; }
+        public DateTime LastLoginDateTime { get; set; }
+    }
+}
